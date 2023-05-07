0c5229776128a37a5a9b4170c2038855f9864156
unknown <islam.shaaban13@gmail.com>
2023-05-07 22:34:21 +0300

    add authintication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
